101ccd176737a6569ddad42ef4815f3930623556
Aluno SENAI <Aluno@SN-358620.edu.fiesc.com.br>
2024-07-08 12:07:52 -0300

    Enviando

---
+++ formulario.js
@@ -0,0 +1,11 @@
+function enviar(){
+    let nome = document.getElementById('Nome').value
+    let CPF = document.getElementById('CPF').value
+    let Númerodecelular = document.getElementById('Número de celular').value
+    let Estado = document.getElementById('Estado').value
+    let Cidade = document.getElementById('Cidade').value
+    let CEP = document.getElementById('CEP').value
+
+    alert('Nome:'+nome+'\nCPF:'+CPF+'\nNúmero de celular:'+Númerodecelular+'\nEstado:'+Estado+'\nCidade:'+Cidade+'CEP:'+CEP)
+
+}
